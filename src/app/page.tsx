e8ad0d6ee22b3be80e2ac098229b38f0c6911085
Kostia Palchyk <kosich@users.noreply.github.com>
2023-08-13 19:35:59 +0300

    Wip: adding nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Article from './article.mdx';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-24">
+      <div className="max-w-3xl">
+        <Article />
+      </div>
+    </main>
+  );
+}
