82e131cce49dc4ce9ca7dedcc645e35f96ae657b
Kostia Palchyk <kosich@users.noreply.github.com>
2023-08-20 20:09:29 +0300

    fixed mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Contents from './contents.mdx';
 
 export default function Home() {
   return (
-    <section className="min-h-screen flex justify-center">
-      <main className="text-justify flex-grow py-24">
-        <div className="max-w-2xl float-right mx-4">
+    <section className="min-h-screen w-screen flex justify-center">
+      <main className="flex-grow py-24">
+        <div className="text-justify w-screen px-8 lg:w-full lg:ml-auto lg:max-w-2xl">
           <Article />
         </div>
       </main>
-      <aside className="_contents sticky flex-grow top-0 h-screen overflow-auto mx-4 pt-24">
+      <aside className="_contents hidden sticky flex-grow top-0 h-screen overflow-auto mx-4 pt-24 lg:block">
         <div className="w-64 right-0 bottom-0">
           <h2 className="text-lg">Table of contents:</h2>
           <Contents />
